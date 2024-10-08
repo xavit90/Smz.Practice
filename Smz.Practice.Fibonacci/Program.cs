b6b1401eaa5ade6fa31e9d54b8ad672080912e67
Salvador Montes <@smontes>
2024-10-08 15:01:36 -0600

    Ejerccicio Fibonacci

---
+++ Smz.Practice.Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+ */
+ double last = 1;
+ double second = 0;
+
+Console.Write($"{second},{last}");
+ for (int i = 0; i < 50; i++)
+ {
+    double next = second + last;
+    Console.Write($",{next}");
+
+    second = last;
+    last = next;
+ }
--- Smz.Practice.FizzBuzz/Program.cs
+++ Smz.Practice.FizzBuzz/Program.cs
@@ -1,4 +1,12 @@
-﻿for (int i = 1; i <= 100; i++)
+﻿/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+for (int i = 1; i <= 100; i++)
 {
     if (i % 3 == 0 && i % 5 == 0)
         Console.WriteLine("FizzBuzz");
