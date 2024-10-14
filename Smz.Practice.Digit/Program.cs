6d3a7c584b6536cd5e71dcc0962dcc4186d01b53
Salvador Montes <@smontes>
2024-10-14 17:51:34 -0600

    Función de super dígito

--- Smz.Practice.Digit/Program.cs
+++ Smz.Practice.Digit/Program.cs
@@ -6,13 +6,34 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Numero en texto 
         // Console.WriteLine(OperatingSystem.IsWindows());
         if (args.Any())
         {
             if (int.TryParse(args[0], out var number))
+            {
                 Console.WriteLine(NumberToWordsExtension.ToWords(number, new CultureInfo("es")));
+                Console.WriteLine($"Super Dígito: {SuperDigito(number)}");
+            }
             else
+            {
                 Console.WriteLine($"\"{args[0]}\" No corresponde a un número");
+            }
         }
+    }
+
+    static int SuperDigito(int numero)
+    {
+        while (numero >= 10)
+        {
+            int suma = 0;
+            while (numero > 0)
+            {
+                suma += numero % 10;
+                numero /= 10;
+            }
+            numero = suma;
+        }
+        return numero;
     }
 }
